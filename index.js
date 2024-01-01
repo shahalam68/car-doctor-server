553e5b0876950145b34886ae012fc76073cb788d
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-01 19:48:32 +0600

    service with id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,12 @@ async function run() {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+    app.get('/services/:id', async( req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await serviceCollection.findOne(query);
+      res.send(result)
     })
 
 
